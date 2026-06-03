09bbe933a9b0172f7da38d5a1dccd7c5053e4b0b
Nguyen An <nguyenan21062005ne@gmail.com>
2026-06-04 05:31:30 +0700

    fix: fix skill and tech

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,30 +1,33 @@
 import SkillTag from "@/ui/SkillTag";
-import csharp from "@/assets/svg/Csharp.svg";
 import java from "@/assets/svg/Java.svg";
 import javascript from "@/assets/svg/JavaScript.svg";
+import typescript from "@/assets/svg/Typescript.svg";
 import nestjs from "@/assets/svg/NestJS.svg";
+import react from "@/assets/svg/React.svg";
+import php from "@/assets/svg/PHP.svg";
+import mysql from "@/assets/svg/MySQL.svg";
+import postgreSQL from "@/assets/svg/PostgreSQL.svg";
+
 import git from "@/assets/svg/Git.svg";
 import vscode from "@/assets/svg/Vscode.svg"
 
 
 function Skill() {
     const listSkill = [
-        { path: csharp, content: "CSharp" },
         { path: java, content: "Java" },
         { path: javascript, content: "JavaScript" },
+        { path: typescript, content: "TypeScript" },
         { path: nestjs, content: "NestJS" },
-        { path: csharp, content: "CSharp" },
-        { path: java, content: "Java" },
-        { path: javascript, content: "JavaScript" },
-        { path: nestjs, content: "NestJS" },
-        { path: csharp, content: "CSharp" },
-        { path: java, content: "Java" },
-        { path: javascript, content: "JavaScript" },
-        { path: nestjs, content: "NestJS" },
+        { path: react, content: "React" },
+        { path: php, content: "PHP" },
+        { path: mysql, content: "MySQL" },
+        { path: postgreSQL, content: "PostgreSQL" },
+        
     ];
     const listTool = [
         { path: git, content: "Git" },
         { path: vscode, content: "Vs Code" },
+        { path: postgreSQL, content: "pgAdmin" },
     ];
 
     return (
